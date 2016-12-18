1fe0702e610880bfdd823aafc47829b139a851b0
Luke Melaia <lukemelaia.personal@gmail.com>
2016-12-18 23:32:22 +0200

    Added launch4j build files

---
+++ buildSrc/src/main/java/org/lmelaia/iseries/build/launch4j/JavaArchitecture.java
@@ -0,0 +1,43 @@
+/*   Copyright (C) 2016  Luke Melaia
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.lmelaia.iseries.build.launch4j;
+
+/**
+ * The java runtime environment architecture launch4j will look for.
+ *
+ * @author Luke Melaia
+ */
+public enum JavaArchitecture {
+
+    /**
+     * Only 64bit architecture will be used.
+     */
+    ONLY_64BIT,
+    /**
+     * 64Bit architecture will be used first, if available, otherwise 32bit will
+     * be used.
+     */
+    _64BIT_THEN_32BIT,
+    /**
+     * 32Bit architecture will be used first, if available, otherwise 64bit will
+     * be used.
+     */
+    _32BIT_THEN_64BIT,
+    /**
+     * Only 32bit architecture will be used.
+     */
+    ONLY_32BIT;
+}
