1535695ebd89fdcdd37d859e0122e19a0ff76039
Luke Melaia <lukemelaia.personal@gmail.com>
2016-12-29 19:32:08 +0200

    Edited javadoc

--- buildSrc/src/main/java/org/lmelaia/iseries/build/launch4j/Launch4jConfigurationBuilder.java
+++ buildSrc/src/main/java/org/lmelaia/iseries/build/launch4j/Launch4jConfigurationBuilder.java
@@ -312,7 +312,7 @@ public class Launch4jConfigurationBuilder {
     protected String[] objectFiles;
 
     /**
-     * The API files used by launch4j.
+     * The windows 32 API files used by launch4j.
      *
      * <p>
      * This option <b>may</b> be {@code null} and is set to {@code null} by
