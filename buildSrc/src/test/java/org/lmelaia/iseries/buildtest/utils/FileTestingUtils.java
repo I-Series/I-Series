a462aff3c172643e58845c02ff4290f98ddc1324
Luke Melaia <lukemelaia.personal@gmail.com>
2017-01-04 14:26:56 +0200

    Refactored FileTestingUtils and included a way to get a file from an ID.

--- buildSrc/src/test/java/org/lmelaia/iseries/buildtest/utils/FileTestingUtils.java
+++ buildSrc/src/test/java/org/lmelaia/iseries/buildtest/utils/FileTestingUtils.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -40,14 +41,9 @@ public class FileTestingUtils {
             = System.getProperty("user.dir") + "\\tests\\";
 
     /**
-     * The files.
+     * A list of the test files.
      */
-    private File files[] = null;
-
-    /**
-     * The content of each file.
-     */
-    private String[] contents;
+    private List<TestFile> files = new ArrayList();
 
     /**
      * See {@link #FileUtils(java.lang.String[][])) for full documentation.
@@ -57,20 +53,64 @@ public class FileTestingUtils {
      */
     public FileTestingUtils(String path, String[][] files) {
         new File(path).mkdirs();
-        this.files = getFiles(Objects.requireNonNull(files), path);
-        this.contents = getContent(files);
+        validatePath(path);
+        this.files.clear();
+
+        for (String[] fileProps : files) {
+            TestFile testFile = new TestFile(fileProps[0]).initReference(path);
+            switch (fileProps.length) {
+                case 2:
+                    testFile.setContent(fileProps[1]);
+                    break;
+                case 3:
+                    testFile.setContent(fileProps[1]).setID(fileProps[2]);
+                    break;
+            }
+            this.files.add(testFile);
+        }
     }
 
     /**
-     * Creates a new FileUtils object with a default home directory of {@link
-     * #PATH_BUILD_TEST_FILES}, and a list of files and there content.
+     * Creates a new FileTestingUtils object with a default home directory of
+     * {@link #PATH_BUILD_TEST_FILES}, and a list of files and there content.
      *
-     * @param files an array of arrays, each containing a file name as their
-     * first element, and there content as the second element. The second
-     * element can be null if the file doesn't need content.
+     * @param files An array where each element is another array which specifies
+     * the the properties of a test file. Within each sub-array should be 1 to 3
+     * Strings - the first is the name of the file (only the name and extension,
+     * not the path) which is required, the second is the content which will be
+     * written to the file (this isn't required and can be null), the final
+     * String is the files ID - this is used to get a reference to the file
+     * easily, this isn't required and cannot be null. If an ID is not
+     * specified, the ID is set to the files name (i.e. the first element).
      */
     public FileTestingUtils(String[][] files) {
         this(PATH_BUILD_TEST_FILES, files);
+    }
+
+    /**
+     * See {@link #FileTestingUtils(java.lang.String[][])} for full
+     * documentation.
+     *
+     * @param path the home directory of the test files
+     * @param testFiles a list of test files.
+     */
+    public FileTestingUtils(String path, TestFile... testFiles) {
+        new File(path).mkdirs();
+        validatePath(path);
+
+        files.clear();
+        files.addAll(Arrays.asList(testFiles));
+        files.forEach(file -> {file.initReference(path);});
+    }
+
+    /**
+     * Creates a new FileTestingUtils object with a list of test files which can
+     * be created and deleted with a method call.
+     *
+     * @param testFiles a list of test files
+     */
+    public FileTestingUtils(TestFile... testFiles) {
+        this(PATH_BUILD_TEST_FILES, testFiles);
     }
 
     /**
@@ -79,17 +119,22 @@ public class FileTestingUtils {
      * @throws IOException if the file cannot be created or written to.
      */
     public void setup() throws IOException {
-        for (int i = 0; i < files.length; i++) {
-            File file = files[i];
-            String content = contents[i];
+        for (TestFile testFile : files) {
+            File ref = testFile.reference;
+            String content = testFile.content;
 
-            file.createNewFile();
+            if (!ref.createNewFile()) {
+                System.err.println("Couldn't create file: " + ref);
+                continue;
+            }
 
-            if (content != null) {
-                try (BufferedWriter writer = new BufferedWriter(
-                        new FileWriter(file))) {
-                    writer.write(content);
-                }
+            if (content == null) {
+                continue;
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(
+                    new FileWriter(ref))) {
+                writer.write(content);
             }
         }
     }
@@ -103,8 +148,8 @@ public class FileTestingUtils {
     public boolean teardown() {
         boolean success = true;
 
-        for (File file : files) {
-            if (!file.delete()) {
+        for (TestFile tFile : files) {
+            if (!tFile.reference.delete()) {
                 success = false;
             }
         }
@@ -113,56 +158,112 @@ public class FileTestingUtils {
     }
 
     /**
-     * Gets the content out of the multidimensional array.
+     * Returns a file object with
      *
-     * @param names
-     * @return
+     * @param id the ID of a test file.
+     * @return the file reference of a test file with the ID passed as an
+     * argument or {@code null} if no test file exists with the ID passed in.
      */
-    private String[] getContent(String[][] names) {
-        List<String> ret = new ArrayList<>();
-
-        for (String[] element : names) {
-            if (element[1] != null && element[1].equals("")) {
-                element[1] = null;
+    public File getFileByID(String id) {
+        for (TestFile file : files) {
+            if (file.id.toLowerCase().equals(id.toLowerCase())) {
+                return file.reference;
             }
-            ret.add(element[1]);
         }
 
-        return ret.toArray(new String[ret.size()]);
+        return null;
     }
 
     /**
-     * Gets the file names out the multidimensional array and turns them into
-     * valid file objects.
+     * Ensures the path ends with a forward slash({@code "/"}).
      *
-     * @param names
-     * @param path the home directory of the files.
-     * @return
+     * @param path the path.S
      */
-    private File[] getFiles(String[][] names, String path) {
-        List<File> ret = new ArrayList<>();
+    private void validatePath(String path) {
+        if (!path.endsWith("/") || !path.endsWith("\\")) {
+            path += "/";
+        }
+    }
 
-        for (int i = 0; i < names.length; i++) {
-            String[] element = names[i];
-            if(element.length == 1){
-                element = new String[]{element[0], null};
-            }
-            
-            if (element.length != 2) {
-                throw new IllegalArgumentException("Each array must have "
-                        + "one or two elements.");
-            }
+    /**
+     * An instance of this class would represent a single file used by a unit
+     * test(s) or class of unit tests.
+     *
+     * <p>
+     * The file will be created when it's needed and deleted once the unit tests
+     * which require it have been run.</p>
+     */
+    public static class TestFile {
 
-            if (element[0] == null || element[0].isEmpty()) {
-                throw new IllegalArgumentException(
-                        "The first element in each array must "
-                        + "not be null or empty.");
-            }
+        /**
+         * The name of the file. This is just the file name and its extension,
+         * the path is not included in the name.
+         */
+        private final String name;
 
-            names[i] = element;
-            ret.add(new File(path + element[0]));
+        /**
+         * The content of the file. Can be null
+         */
+        private String content;
+
+        /**
+         * The files ID. Can be null
+         */
+        private String id;
+
+        /**
+         * The actual file this object points to.
+         */
+        private File reference;
+
+        /**
+         * Constructs a new test file instance with the specified name.
+         *
+         * @param name the name of the file not including the path to it (e.g.
+         * TestFile.txt)
+         */
+        public TestFile(String name) {
+            this.id = Objects.requireNonNull(name);
+            this.name = id;
         }
 
-        return ret.toArray(new File[ret.size()]);
+        /**
+         * Sets the content of the file. This isn't required.
+         *
+         * @param content the content of the file.
+         * @return a reference to this object.
+         */
+        public TestFile setContent(String content) {
+            this.content = content;
+            return this;
+        }
+
+        /**
+         * Sets the ID of the file. This isn't required.
+         *
+         * <p>
+         * <b>
+         * If an ID is not specified, the ID of the file will become its name
+         * (including the extension but not including the path).
+         * </b></p>
+         *
+         * @param id the files ID.
+         * @return a reference to this object.
+         */
+        public TestFile setID(String id) {
+            this.id = Objects.requireNonNull(id);
+            return this;
+        }
+
+        /**
+         * Initializes the file reference in this class using the path provided.
+         *
+         * @param path the path to the folder which will contain the test files.
+         * @return a reference to this object.
+         */
+        private TestFile initReference(String path) {
+            this.reference = new File(Objects.requireNonNull(path) + name);
+            return this;
+        }
     }
 }
--- buildSrc/src/test/java/org/lmelaia/iseries/buildtest/utils/TestFileTestingUtils.java
+++ buildSrc/src/test/java/org/lmelaia/iseries/buildtest/utils/TestFileTestingUtils.java
@@ -22,60 +22,128 @@ import java.io.IOException;
 import static org.junit.Assert.fail;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.lmelaia.iseries.buildtest.utils.FileTestingUtils.TestFile;
 
 /**
  * Tests the {@link FileTestingUtils} class.
- * 
+ *
  * @author Luke Melaia
  */
 public class TestFileTestingUtils {
-    
+
     private static FileTestingUtils fileUtils;
-    
+
+    private static FileTestingUtils fileUtilsNew;
+
     private static String file1 = "TestFileOne.test",
             file1Content = "Some content...",
             file2 = "TestFileTwo.test",
+            file3 = "TestFileThree.test",
+            file3Content = "Some more content...",
             path = System.getProperty("user.dir") + "\\tests\\";
-    
+
     @BeforeClass
-    public static void setup(){
+    public static void setup() {
         fileUtils = new FileTestingUtils(path, new String[][]{
             new String[]{file1, file1Content},
-            new String[]{file2}});
+            new String[]{file2},
+            new String[]{file3, file3Content}
+        });
+
+        fileUtilsNew = new FileTestingUtils(
+                new TestFile("TestFileA.test").setContent("A bit of content"),
+                new TestFile("TestFileB.test").setContent("Some content here"),
+                new TestFile("TestFileC.test")
+                .setContent("Some more content")
+                .setID("tfc"),
+                new TestFile("TestFileD.test").setID("tfd")
+        );
     }
-    
+
     @Test
-    public void testSetup() throws IOException{
+    public void testSetup() throws IOException {
         fileUtils.setup();
         
-        if(!new File(path + file1).exists()){
+        if (!new File(path + file1).exists()) {
             fail("The file " + path + file1 + " wasn't created");
         }
-        
-        try(BufferedReader br = new BufferedReader(new FileReader(
+
+        try (BufferedReader br = new BufferedReader(new FileReader(
                 new File(path + file1)))) {
-            if(!br.readLine().equals(file1Content)){
+            if (!br.readLine().equals(file1Content)) {
+                fail("The file was not written to");
+            }
+        }
+
+        if (!new File(path + file2).exists()) {
+            fail("The file " + path + file1 + " wasn't created");
+        }
+
+        if (!new File(path + file3).exists()) {
+            fail("The file " + path + file3 + " wasn't created");
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(
+                new File(path + file3)))) {
+            if (!br.readLine().equals(file3Content)) {
                 fail("The file was not written to");
             }
         }
         
-        if(!new File(path + file2).exists()){
-            fail("The file " + path + file1 + " wasn't created");
+        if(!fileUtils.getFileByID(file2).exists()){
+            fail("Couldn't get file by ID using String Array constructor");
+        }
+        
+        fileUtilsNew.setup();
+        
+        if(!fileUtilsNew.getFileByID("TestFileA.test").exists()){
+            fail("The file TestFileA.test wasn't created");
+        }
+        
+        if(!fileUtilsNew.getFileByID("TestFileB.test").exists()){
+            fail("The file TestFileB.test wasn't created");
+        }
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(
+                fileUtilsNew.getFileByID("TestFileB.test")))) {
+            if (!br.readLine().equals("Some content here")) {
+                fail("The file TestFileB.test was not written to");
+            }
+        }
+        
+        if(!fileUtilsNew.getFileByID("tfc").exists()){
+            fail("The file TestFileC.test wasn't created or getting a file by"
+                    + " doesn't work");
+        }
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(
+                fileUtilsNew.getFileByID("tfc")))) {
+            if (!br.readLine().equals("Some more content")) {
+                fail("The file TestFileC.test was not written to");
+            }
         }
     }
-    
+
     @Test
-    public void testTeardown(){
-        fileUtils.teardown();
+    public void testTeardown() {
+        boolean a = fileUtils.teardown();
+        boolean b = fileUtilsNew.teardown();
         //@REM: Here because of bug
         System.out.println("new File(path + file1).exists()" + new File(path + file1).exists());
-        if(new File(path + file1).exists()){
+        if (new File(path + file1).exists()) {
             fail("The file " + path + file1 + " wasn't deleted");
         }
         //@REM: Here because of bug
-        System.out.println("new File(path + file1).exists()" + new File(path + file2).exists());
-        if(new File(path + file2).exists()){
-            fail("The file " + path + file1 + " wasn't deleted"); 
+        System.out.println("new File(path + file2).exists()" + new File(path + file2).exists());
+        if (new File(path + file2).exists()) {
+            fail("The file " + path + file2 + " wasn't deleted");
         }
+        //@REM: Here because of bug
+        System.out.println("new File(path + file3).exists()" + new File(path + file3).exists());
+        if (new File(path + file3).exists()) {
+            fail("The file " + path + file3 + " wasn't deleted");
+        }
+        
+        System.out.println("A: " + a + "\nB: " + b);
     }
 }
