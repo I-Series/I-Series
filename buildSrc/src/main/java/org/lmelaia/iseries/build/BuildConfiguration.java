058ae4888c36daf8c58b53dd1208605739837cc6
Luke Melaia <>
2017-05-25 23:15:20 +0200

    Added and implemented the ability to package the build output to a zip file.

--- buildSrc/src/main/java/org/lmelaia/iseries/build/BuildConfiguration.java
+++ buildSrc/src/main/java/org/lmelaia/iseries/build/BuildConfiguration.java
@@ -29,6 +29,7 @@ import org.lmelaia.iseries.build.launch4j.Launch4jProcessWrapper;
 import org.lmelaia.iseries.build.library.Library;
 import org.lmelaia.iseries.build.library.LibraryManager;
 import org.lmelaia.iseries.build.licence.Licences;
+import org.lmelaia.iseries.build.packaging.ZipPackager;
 import org.lmelaia.iseries.build.utils.CopyFile;
 import org.lmelaia.iseries.build.utils.OutputCopyFile;
 
@@ -45,6 +46,20 @@ public class BuildConfiguration {
     //******************************
     
     //******************************
+    //             NAMES
+    //******************************
+    /**
+     * The name of the application.
+     */
+    private static final String APPLICATION_NAME = "I-Series";
+
+    /**
+     * The name of the application with a '.exe' extension. Used as the launch4j
+     * executable file name.
+     */
+    private static final String EXECUTABLE_NAME = APPLICATION_NAME + ".exe";
+
+    //******************************
     //     FILES AND FILE PATHS
     //******************************
     
@@ -53,19 +68,20 @@ public class BuildConfiguration {
      */
     public static final String PROJECT_PATH
             = "C:/Users/Luke/Projects/Java/I-Series/";
-
+    //Change this to the path on your machine.
+    
     /**
      * The I-Series project folder.
      */
     public static final File PROJECT_FOLDER = new File(PROJECT_PATH);
-    
+
     /**
      * The string representation of the path to the I-Series jar file.
      */
     public static final String JAR_PATH
             = PROJECT_PATH
             + "build/libs/I-Series.jar";
-    
+
     /**
      * The I-Series jar file.
      */
@@ -75,8 +91,8 @@ public class BuildConfiguration {
      * The string representation of the path to where the build output will be
      * written.
      */
-    public static final String OUTPUT_PATH = PROJECT_PATH + "output/";
-    
+    public static final String OUTPUT_PATH = PROJECT_PATH + "buildoutput/";
+
     /**
      * The final build output folder.
      */
@@ -88,7 +104,7 @@ public class BuildConfiguration {
     //Change this to the path of launch4j on your machine.
     public static final String LAUNCH4J_PATH
             = "C:/Program Files (x86)/Launch4j";
-    
+
     /**
      * The launch4j folder which contains the launch4j binaries and executables.
      */
@@ -105,54 +121,74 @@ public class BuildConfiguration {
      * The folder containing the licences files.
      */
     public static final File LICENCES_FOLDER = new File(LICENCES_PATH);
-    
+
     /**
      * The path to the folder containing the library jar files.
      */
     public static final String LIBRARIES_PATH
             = PROJECT_PATH + "build/libs/libs/";
-    
+
     /**
      * The folder containing the library jar files.
      */
     public static final File LIBRARIES_FOLDER = new File(LIBRARIES_PATH);
-    
+
     /**
      * The path to the legal folder inside the output folder. This folder
      * contains the licences for the libraries.
      */
     public static final String LEGAL_PATH = OUTPUT_PATH + "legal/";
-    
+
     /**
      * The folder containing the licences for the libraries.
      */
     public static final File LEGAL_FOLDER = new File(LEGAL_PATH);
-    
+
+    /**
+     * The path to the distribution folder.
+     */
+    public static final String DISTRIBUTION_PATH
+            = PROJECT_PATH + "distribution/";
+
+    /**
+     * The distribution folder. Contains the files which will be distributed
+     * to the user.
+     */
+    public static final File DISTRIBUTION_FOLDER = new File(DISTRIBUTION_PATH);
+
     /**
      * The path to the folder containing the copied libraries.
      */
     public static final String OUT_LIBRARIES_PATH = OUTPUT_PATH + "libs/";
-    
+
     /**
      * The folder containing the copied libraries.
      */
-    public static final File OUT_LIBRARIES_FOLDER 
+    public static final File OUT_LIBRARIES_FOLDER
             = new File(OUT_LIBRARIES_PATH);
-    
-    //******************************
-    //             NAMES
-    //******************************
-    
-    /**
-     * The name of the application.
-     */
-    private static final String APPLICATION_NAME = "I-Series";
 
     /**
-     * The name of the application with a '.exe' extension. Used as the launch4j
-     * executable file name.
+     * The path to the windows distribution zip file
      */
-    private static final String EXECUTABLE_NAME = APPLICATION_NAME + ".exe";
+    public static final String WINDOWS_ZIP_PATH = DISTRIBUTION_PATH
+            + APPLICATION_NAME + " Windows.zip";
+
+    /**
+     * The windows distribution file.
+     */
+    public static final File WINDOWS_ZIP_FILE = new File(WINDOWS_ZIP_PATH);
+
+    /**
+     * The path to the cross-platform distribution zip file.
+     */
+    public static final String CROSSPLATFORM_ZIP_PATH = DISTRIBUTION_PATH
+            + APPLICATION_NAME + " Cross-platform.zip";
+
+    /**
+     * The cross-platform distribution zip file.
+     */
+    public static final File CROSSPLATFORM_ZIP_FILE
+            = new File(CROSSPLATFORM_ZIP_PATH);
 
     //******************************
     //        CONFIGURATION
@@ -163,21 +199,21 @@ public class BuildConfiguration {
      */
     private static Launch4jConfiguration executableConfiguration
             = new Launch4jConfigurationBuilder()
-            .setJarRuntimePath("I-Series.jar")
-            .setWrap(false)
-            .setOutputFile(OUTPUT_PATH + EXECUTABLE_NAME)
-            .setMinimumJreVersion("1.8.0_111")
-            .create();
+                    .setJarRuntimePath("I-Series.jar")
+                    .setWrap(false)
+                    .setOutputFile(OUTPUT_PATH + EXECUTABLE_NAME)
+                    .setMinimumJreVersion("1.8.0_111")
+                    .create();
 
     /**
-     * Contains a list of the libraries for the application and handles
-     * the copies of them and their licences.
+     * Contains a list of the libraries for the application and handles the
+     * copies of them and their licences.
      */
     private static final LibraryManager LIBRARY_MANAGER = new LibraryManager(
-                    new File(PROJECT_PATH + "build/libs/libs"),
-                    new File(OUTPUT_PATH + "libs/"),
-                    new File(OUTPUT_PATH + "legal/"));
-    
+            new File(PROJECT_PATH + "build/libs/libs"),
+            new File(OUTPUT_PATH + "libs/"),
+            new File(OUTPUT_PATH + "legal/"));
+
     //******************************
     //             LISTS
     //******************************
@@ -193,21 +229,21 @@ public class BuildConfiguration {
         new OutputCopyFile(Licences.GNU.getFile(),
         APPLICATION_NAME + " Licence.txt")
     };
-    
+
     /**
      * A list of the libraries for the root project.
      */
     private static final Library[] LIBRARIES = {
         new Library("Gson", "gson-2.8.0", Licences.APACHE)
     };
-    
+
     /**
      * A list of directories required to exist before beginning a full build.
      */
     private static final File[] REQUIRED_DIRECTORIES = {
-        OUTPUT_FOLDER, LEGAL_FOLDER, OUT_LIBRARIES_FOLDER
+        OUTPUT_FOLDER, LEGAL_FOLDER, OUT_LIBRARIES_FOLDER, DISTRIBUTION_FOLDER
     };
-    
+
     //*******************
     //      METHODS
     //*******************
@@ -219,13 +255,13 @@ public class BuildConfiguration {
         System.out.println("Creating I-Series executable");
         createExecutable(executableConfiguration);
     }
-    
+
     /**
      * Adds the list of libraries ({@link #LIBRARIES) to the library manager.
      */
-    private static void addLibrariesToList(){
-        for(Library library : LIBRARIES){
-            try{
+    private static void addLibrariesToList() {
+        for (Library library : LIBRARIES) {
+            try {
                 LIBRARY_MANAGER.addLibrary(library);
             } catch (FileNotFoundException ex) {
                 System.err.println("Failed to add library: "
@@ -233,40 +269,41 @@ public class BuildConfiguration {
             }
         }
     }
-    
+
     /**
      * Creates the directories required to do a full build.
      */
-    private static void createRequiredDirectories(){
-        for(File f : REQUIRED_DIRECTORIES){
-            if(!forceCleanMake(f)){
+    private static void createRequiredDirectories() {
+        for (File f : REQUIRED_DIRECTORIES) {
+            if (!forceCleanMake(f)) {
                 System.err.println("Failed to create/clean directory: " + f);
             }
         }
     }
-    
+
     /**
      * Makes a directory, including any necessary but nonexistent parent
-     * directories if the directory is nonexistent, else the directory
-     * is cleaned.
-     * 
+     * directories if the directory is nonexistent, else the directory is
+     * cleaned.
+     *
      * @param f the directory
      * @return {@code true} if and only if the directory was created or cleaned,
      * {@code false} otherwise.
      */
-    private static boolean forceCleanMake(File f){
-        try{
-            if(f.exists())
+    private static boolean forceCleanMake(File f) {
+        try {
+            if (f.exists()) {
                 FileUtils.cleanDirectory(f);
-             else 
+            } else {
                 FileUtils.forceMkdir(f);
-        } catch (IOException ex){
+            }
+        } catch (IOException ex) {
             return false;
         }
-        
+
         return true;
     }
-    
+
     /**
      * Uses the library manager to copy the libraries and library licences.
      */
@@ -316,13 +353,41 @@ public class BuildConfiguration {
                     Arrays.toString(ex.getStackTrace()).replaceAll(",", ",\n"));
         }
 
-        if(!output.toString().equals("launch4j: Compiling resources")){
+        if (!output.toString().equals("launch4j: Compiling resources")) {
             System.err.println("Launch4j might have failed "
                     + "to produce an executable");
         }
-        
+
         System.out.println("Launch4j output: " + (output.toString().equals("")
                 ? "No output" : output.toString()));
+    }
+
+    /**
+     * Zips the projects build output into a windows and a cross-platform
+     * zip file.
+     * 
+     * @throws IOException 
+     */
+    private static void zipProject() throws IOException {
+        //Create a copy of the output folder without
+        //windows executables for packaging
+        //into the cross-platform zip.
+        File nwof = new File(OUTPUT_PATH.substring(0, OUTPUT_PATH.length() - 1)
+                + "nw");
+        FileUtils.copyDirectory(OUTPUT_FOLDER, nwof);
+        new File(nwof.getCanonicalPath() + "/" + APPLICATION_NAME + ".exe")
+                .delete();
+
+        //Zip files
+        ZipPackager windows = new ZipPackager(WINDOWS_ZIP_FILE, OUTPUT_FOLDER);
+        windows.create();
+
+        ZipPackager crossPlatform = new ZipPackager(
+                CROSSPLATFORM_ZIP_FILE, nwof);
+        crossPlatform.create();
+
+        //Delete output copy
+        FileUtils.deleteDirectory(nwof);
     }
 
     /**
@@ -370,7 +435,7 @@ public class BuildConfiguration {
 
         FileUtils.copyFile(source, dest);
     }
-    
+
     /**
      * This method is considered an alternative to
      * {@link #main(java.lang.String[])}, and is called ONLY when the user whats
@@ -394,6 +459,10 @@ public class BuildConfiguration {
         addLibrariesToList();
         copyLibraries();
         buildISeriesExecutable();
+        //Give windows explorer time to refresh
+        try{Thread.sleep(1000);}
+        catch(Exception ex){}
+        zipProject();
     }
 
     /**
